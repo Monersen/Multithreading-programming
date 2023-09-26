c4dd4616d06cd56b122b6eb82d0d90de1d1501bd
Александр Канов <monersen@gmail.com>
2023-09-26 05:58:33 +0300

    Асинхронное программирование

---
+++ 3.1/3.1.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <thread>
+#include <future>
+#include <vector>
+#include <random>
+#include <algorithm>
+using namespace std;
+
+template<typename T>
+void min_idx(const T& v,
+    int start_idx,
+    promise<int>&& pr)
+{
+    auto m = start_idx;
+    for (; start_idx < v.size(); ++start_idx)
+        if (v[start_idx] < v[m])
+            m = start_idx;
+
+    pr.set_value(m);
+}
+
+template<typename T>
+void selectionSort(T& arr, size_t size)
+{
+    for (int i = 0; i < size; ++i)
+    {
+        promise<int> pr;
+        future<int> ft_res = pr.get_future();
+        future<void> task = async(launch::async, min_idx<T>, arr, i, move(pr));
+        auto midx = ft_res.get();
+        swap(arr[i], arr[midx]);
+    }
+}
+
+int main()
+{
+    vector v(10, 0);
+    mt19937 gen(time(nullptr));
+    uniform_int_distribution<int> dis(0, 10);
+    auto rand_num([=]() mutable { return dis(gen); });
+    generate(begin(v), end(v), rand_num);
+
+    selectionSort(v, v.size());
+
+    for (const auto& i : v)
+        cout << i << " ";
+
+    cout << endl;
+    return 0;
+}
---
+++ 3.2/3.2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <functional>
+#include <future>
+using namespace std;
+
+const unsigned long MAX_CHUNK_SIZE = 2;
+
+template<class Iterator, class UnaryFunction>
+void foreach(Iterator first, Iterator last, UnaryFunction fx)
+{
+    unsigned long const len = distance(first, last);
+
+    if (len <= MAX_CHUNK_SIZE)
+    {
+        for (; first != last; ++first)
+            fx(*first);
+    }
+    else
+    {
+        Iterator mid_point = first;
+        advance(mid_point, len / 2);
+        future<void> first_half_res = async(launch::async,
+            foreach<Iterator, UnaryFunction>,
+            first,
+            mid_point,
+            fx);
+
+        future<void> second_half_res = async(launch::async,
+            foreach<Iterator, UnaryFunction>,
+            mid_point,
+            last,
+            fx);
+        first_half_res.wait();
+        second_half_res.wait();
+    }
+}
+
+int main()
+{
+    vector<int> v(10, 5);
+    int x = 0;
+    foreach(v.begin(), v.end(), [&x](int n) { x += n; });
+    cout << x << endl;
+    return 0;
+}
